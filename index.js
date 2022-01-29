e6da2988cbb4140b6aff960afcc882da6d9f6e4b
Soham Sarkar <soham.ss1@gmail.com>
2022-01-29 14:09:09 +0530

    feat: v1 static jobs listings

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jobContainer = document.getElementById('jobs')
+const selectedFiltersContainer = document.querySelector('.searchBox')
 // get all jobs data
 const getJobs = async () => await (await fetch('./data.json')).json()
 
@@ -7,14 +8,33 @@ const FILTER_KEY = KEY + 'filters'
 
 const filterState = JSON.parse(window.localStorage.getItem(FILTER_KEY)) ?? []
 
-console.log(filterState)
-
 // create job card
 const createJobCard = () => {
   getJobs().then(jobs => {
     let cardTemplate = ``
     for (job of jobs) {
-      cardTemplate += `<div class="job">
+      if (filterState.length) {
+        console.log(
+          job,
+          filterState,
+          'role',
+          filterState.includes(job.role),
+          'lang',
+          filterState.some(el => job.languages.indexOf(el) >= 0),
+          'tools',
+          filterState.some(el => job.tools.indexOf(el) >= 0),
+        )
+        const hasRole = filterState.includes(job.role)
+        const hasLanguage = filterState.some(
+          el => job.languages.indexOf(el) >= 0,
+        )
+        const hasTool = filterState.some(el => job.tools.indexOf(el) >= 0)
+        const filterCondition = hasRole || hasLanguage || hasTool
+
+        console.log(!filterCondition)
+        if (!filterCondition) continue
+      }
+      cardTemplate += `<div class="job ${job.featured ? 'leftBorder' : ''}">
       <div class="leftFlex">
         <img src=${job.logo} alt="job" class="jobImage" />
         <div class="info">
@@ -49,27 +69,77 @@ const createJobCard = () => {
 
 const createFilters = ({role, tools, languages}) => {
   return `<div class="filter" onclick="handleAddFilter(this)">${role}</div>
-  ${tools.map(tool => `<div class="filter">${tool}</div>`).join('')}
-  ${languages.map(lang => `<div class="filter">${lang}</div>`).join('')}
+  ${tools
+    .map(
+      tool =>
+        `<div class="filter" onclick="handleAddFilter(this)">${tool}</div>`,
+    )
+    .join('')}
+  ${languages
+    .map(
+      lang =>
+        `<div class="filter" onclick="handleAddFilter(this)">${lang}</div>`,
+    )
+    .join('')}
   </div>`
 }
 
 createJobCard()
 
-// const filters = document.querySelectorAll('.filter')
-// console.log(filters)
-
-// filters.forEach(filter => {
-//   filter.addEventListener('click', function () {
-//     console.log({filter})
-//   })
-// })
-
-function handleAddFilter(e) {
+const handleAddFilter = e => {
   const value = e.textContent
   if (!filterState.includes(value)) {
     filterState.push(value)
   }
-  console.log(filterState)
+  createSelectedFilters()
+  createJobCard()
+  updateLocalStorage()
+}
+
+const updateLocalStorage = () => {
   window.localStorage.setItem(FILTER_KEY, JSON.stringify(filterState))
+}
+
+const createSelectedFilters = () => {
+  if (!filterState.length) {
+    selectedFiltersContainer.classList.add('hide')
+  } else {
+    selectedFiltersContainer.classList.remove('hide')
+  }
+  let template = ``
+  template += ` <div class="selectedFiltersBox">
+  ${filterState
+    .map(
+      filter => `<div class="selectedFilterWrapper">
+  <div class="selectedFilter">${filter}</div>
+  <div class="removeIconBox" onclick="handleRemoveFilter(this)">
+    <img
+      src="./images/icon-remove.svg"
+      alt="remove filter"
+      class="removeIcon"
+      width="14"
+      height="14"
+    />
+  </div>
+  </div>`,
+    )
+    .join('')}
+</div>
+<button class="clearBtn" onclick="handleRemoveFilter(this,true)">Clear</button>`
+
+  selectedFiltersContainer.innerHTML = template
+}
+
+createSelectedFilters()
+
+// createSelectedFilters()
+const handleRemoveFilter = (e, shouldRemoveAll) => {
+  if (shouldRemoveAll) {
+    filterState.splice(0, filterState.length)
+  } else {
+    filterState.splice(filterState.indexOf(e.textContent), 1)
+  }
+  createSelectedFilters()
+  createJobCard()
+  updateLocalStorage()
 }
